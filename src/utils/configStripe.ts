d26cec1fd4d814081fbf5dd13c72d4659a68cf8a
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 15:30:59 -0700

    fix: type problem

--- src/utils/configStripe.ts
+++ src/utils/configStripe.ts
@@ -3,6 +3,7 @@ import Stripe from "stripe";
 export const connectionStripe = () => {
   //@ts-ignore
   return new Stripe(process.env.STRIPE_KEY, {
+    //@ts-ignore
     apiVersion: process.env.STRIPE_API_VERSION,
   });
 };
