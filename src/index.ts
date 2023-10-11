e6a8d6832a5b1d4578b6bf5fd125324342de88dd
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 15:22:15 -0700

    fix: add tsc

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,19 @@ import "dotenv/config";
 // import bodyParser from "body-parser";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://politicozen-dashboard-frontend-p5fagpv5f-rened2america.vercel.app",
+      "https://frontend-politicozen-renemeza.vercel.app",
+      "https://politicozen.dev/",
+      "https://app.politicozen.dev/",
+    ],
+  })
+);
 // app.use(express.json());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
