99eecd7aedd67f83841d0f5921d09386ac75a447
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 16:03:57 -0700

    fix : all petitions cors

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,7 @@ import "dotenv/config";
 // import bodyParser from "body-parser";
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "https://politicozen-dashboard-frontend-p5fagpv5f-rened2america.vercel.app",
-      "https://frontend-politicozen-renemeza.vercel.app",
-      "https://politicozen.dev/",
-      "https://app.politicozen.dev/",
-      "https://app.politicozen.dev/login",
-    ],
-    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
 // app.use(express.json());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
