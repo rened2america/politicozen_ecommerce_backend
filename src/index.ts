b6c36e785419ad9b64b8d91c75153fafd5e72a42
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 16:08:44 -0700

    fix: more options

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,15 @@ import "dotenv/config";
 // import bodyParser from "body-parser";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+    exposedHeaders: ["authorization"], // you can change the headers
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
 // app.use(express.json());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
