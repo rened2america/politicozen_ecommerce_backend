4d9a543a54bd5506367353485a156ea997695dab
Rene Meza <renemeza.escamilla@gmail.com>
2023-12-01 09:16:56 -0800

    feat: add gallery

--- src/modules/product/productRoute.ts
+++ src/modules/product/productRoute.ts
@@ -2,15 +2,30 @@ import { Router } from "express";
 import { productController } from "./productController";
 import { authValidate } from "../../middlewares/authMiddlewares";
 import express from "express";
-// import { raw } from "body-parser";
+const multer = require("multer");
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024, // limit file size to 10MB
+  },
+});
 const routes = Router();
 
 routes
   .post("/create", authValidate, productController.create)
   .get("/all", productController.getAll)
+  .get("/groupRelation", productController.getGroupRelation)
+  .get("/groupRelation/:artist", productController.getGroupRelation)
   .get("/allByUser", authValidate, productController.getByUser)
   .post("/payment", productController.session)
   .get("/infoorders/orders", authValidate, productController.getOrders)
+  .post(
+    "/upload/art",
+    authValidate,
+    upload.single("art"),
+    productController.createGroup
+  )
+  .get("/gallery", authValidate, productController.getGallery)
   .post(
     "/webhook",
     express.raw({ type: "application/json" }),
